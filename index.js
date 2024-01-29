dfb55b7be99f7579cbad32112fe8b2fdd1425f98
Alex Segura <62962671+Alex-003@users.noreply.github.com>
2024-01-29 16:55:59 +0000

    setting up backend folder

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+
+dotenv.config();
+
+// mongoose no longer supports callback and useNewUrl and useUnified are deprecated
+mongoose
+  .connect(process.env.MONGO_URL, {
+    //useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // optional but recommended
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/user", userRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`);
+});
