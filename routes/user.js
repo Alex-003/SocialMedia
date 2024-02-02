14812d6fc124154e04161dc694dc32d7437c70c8
Alex Segura <62962671+Alex-003@users.noreply.github.com>
2024-02-02 11:22:14 +0000

    update and delete user

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,49 @@
+const User = require("../models/User");
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.send("hey its user route");
+// Update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.body.userId, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json("You can update your account");
+  }
 });
+
+//delete user
+router.delete("/:id", async function (req, res) {
+  if (req.body.userId === req.body.userId || req.body.isAdmin) {
+    try {
+      const user = await User.deleteOne(req.params.id);
+      req.status(200).json("Account has been deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account");
+  }
+});
+
+//get a user
+
+//follow a user
+
+//unfollow a user
 
 module.exports = router;
