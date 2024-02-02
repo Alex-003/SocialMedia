ffc143c47119e7986f7bf5b91ad53592a02f53c0
Alex Segura <62962671+Alex-003@users.noreply.github.com>
2024-02-02 11:28:36 +0000

    get a user by anything

--- routes/user.js
+++ routes/user.js
@@ -42,7 +42,19 @@ router.delete("/:id", async function (req, res) {
 
 //get a user
 
+router.get("/:id", async (req, res) {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other} = user._doc
+    res.status(200).json(other)
+  } catch(err){
+    res.status(500).json(err)
+  }
+})
+
 //follow a user
+
+
 
 //unfollow a user
 
