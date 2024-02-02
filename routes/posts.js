f606a016d0d7c73c9ac15683823db91d46f181d1
Alex Segura <62962671+Alex-003@users.noreply.github.com>
2024-02-02 13:24:21 +0000

    delete, like and dislike post

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { restart } = require("nodemon");
 const Post = require("../models/post");
 module.exports = router;
 
@@ -19,6 +20,8 @@ router.put("/:id", async (req, res) => {
   try {
     const post = Post.findById(req.params.id);
     if (post.userId === req.body.userId) {
+      await post.udpdateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
     } else {
       res.status(403).json("You can update only your post");
     }
@@ -27,7 +30,36 @@ router.put("/:id", async (req, res) => {
   }
 });
 // delete a post
-// like a post
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne({ $set: req.body });
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("You can delete only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// like / dislike a post
+
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("the post has been liked");
+    } else {
+      await post.updateOne({ pull: { likes: req.body.userId } });
+      res.status(200).json("the post has been disliked");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // get a post
 // get timeline posts
 
